24dbe80a381565e894a0b70bfd5195c2dcc93c18
allenac86 <allenac86@gmail.com>
2026-01-22 09:59:05 -0500

    add starter unit tests for frontend, configure vitest, add testing to ci

---
+++ Frontend/app/welcome/welcome.test.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Welcome } from './welcome';
+
+describe('Welcome component', () => {
+  test('renders links for spooky_days_gpt and spooky-days-twitter', () => {
+    render(<Welcome />);
+
+    // Check for the Twitter link
+    const twitterLink = screen.getByRole('link', { name: /spooky_days_gpt/i });
+    expect(twitterLink).toBeInTheDocument();
+    expect(twitterLink).toHaveAttribute('href', 'https://x.com/spooky_days_gpt');
+
+    // Check for the GitHub link
+    const githubLink = screen.getByRole('link', { name: /spooky-days-twitter/i });
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink).toHaveAttribute('href', 'https://www.github.com/allenac86/spooky-days-twitter');
+
+    // Check that SVG icons are present in the links
+    expect(twitterLink.querySelector('svg')).toBeInTheDocument();
+    expect(githubLink.querySelector('svg')).toBeInTheDocument();
+  });
+});
