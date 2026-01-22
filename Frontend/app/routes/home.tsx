8d84929840ab524716ff105bc781a1f94601eda7
allenac86 <allenac86@gmail.com>
2026-01-22 09:26:51 -0500

    add lint to pre-commit hook/ci, format frontend, update default react router layout

--- Frontend/app/routes/home.tsx
+++ Frontend/app/routes/home.tsx
@@ -1,10 +1,10 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { Welcome } from '../welcome/welcome';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "spooky-days-gpt" },
-    { name: "description", content: "Welcome to the Spooky Days Gallery!" },
+    { title: 'spooky-days-gpt' },
+    { name: 'description', content: 'Welcome to the Spooky Days Gallery!' },
   ];
 }
 
---
+++ Frontend/eslint.config.mjs
@@ -0,0 +1,65 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import stylistic from '@stylistic/eslint-plugin';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  {
+    files: ['app/**/*.tsx', 'app/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        React: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      '@stylistic': stylistic,
+      'react': react,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      'no-console': 'off',
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/arrow-spacing': ['error', { 'before': true, 'after': true }],
+      '@stylistic/block-spacing': 'error',
+      '@stylistic/comma-spacing': ['error', { 'before': false, 'after': true }],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/lines-between-class-members': ['error', 'always'],
+      '@stylistic/max-len': ['error', { 'code': 88 }],
+      '@stylistic/max-statements-per-line': ['error', { 'max': 1 }],
+      '@stylistic/multiline-ternary': ['error', 'always'],
+      '@stylistic/newline-per-chained-call': ['error', { 'ignoreChainWithDepth': 2 }],
+      '@stylistic/no-mixed-spaces-and-tabs': 'error',
+      '@stylistic/no-multi-spaces': 'error',
+      '@stylistic/no-multiple-empty-lines': 'error',
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*'},
+        { blankLine: 'never', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      ],
+      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/space-before-blocks': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
