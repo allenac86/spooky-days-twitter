b05f28128fa134cac69fed15666018c6a3c5aa7a
allenac86 <allenac86@gmail.com>
2026-01-22 10:58:26 -0500

    add coverage reporting for frontend tests, add upload of coverage to codecov for frontend, fix security-scan failures

--- Frontend/vitest.config.ts
+++ Frontend/vitest.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    coverage: {
+      reporter: ['lcov', 'text'],
+      exclude: ['node_modules/', '**/*.config.*', '**/*.d.ts'],
+    },
   },
 });
