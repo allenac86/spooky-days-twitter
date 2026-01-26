77561a564a4f8a81861787f677d8d8418d59df89
allenac86 <allenac86@gmail.com>
2026-01-26 17:56:00 -0500

    fix tests after updating github and twitter link text

--- Frontend/app/header/header.test.tsx
+++ Frontend/app/header/header.test.tsx
@@ -7,13 +7,13 @@ describe('Header component', () => {
     render(<Header />);
 
     // Check for the Twitter link
-    const twitterLink = screen.getByRole('link', { name: /spooky_days_gpt/i });
+    const twitterLink = screen.getByRole('link', { name: /twitter/i });
 
     expect(twitterLink).toBeInTheDocument();
     expect(twitterLink).toHaveAttribute('href', 'https://x.com/spooky_days_gpt');
 
     // Check for the GitHub link
-    const githubLink = screen.getByRole('link', { name: /spooky-days-twitter/i });
+    const githubLink = screen.getByRole('link', { name: /github/i });
 
     expect(githubLink).toBeInTheDocument();
     expect(githubLink).toHaveAttribute(
